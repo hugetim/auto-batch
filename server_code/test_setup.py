28294e94e1c7b67ef272d0ec03ded66c7b9a97fd
hugetim@gmail.com <hugetim@gmail.com>
2024-06-24 01:41:30 +0000

    Edited test_setup

--- server_code/test_setup.py
+++ server_code/test_setup.py
@@ -6,5 +6,7 @@ DATA = [dict(text="1")]*60
 
 
 def reset_tables():
-  app_tables.table_2.delete_all_rows()
-  app_tables.table_2.add_rows(DATA)
+    app_tables.table_2.delete_all_rows()
+    new_rows = app_tables.table_2.add_rows(DATA)
+    app_tables.table_1.delete_all_rows()
+    app_tables.table_1.add_row(object="test", rows=new_rows[0:2])
