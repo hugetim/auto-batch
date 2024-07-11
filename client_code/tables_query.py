9da67e69f3d1144d49bd8ae6cefa66a85be9035b
hugetim@gmail.com <hugetim@gmail.com>
2024-07-11 14:12:25 +0000

    Edited tables_query

--- client_code/tables_query.py
+++ client_code/tables_query.py
@@ -1,9 +1,11 @@
 import anvil.tables.query
 from .auto_batch import debatchify
+from functools import wraps
 
 
 def __getattr__(attr):
     if attr in ('all_of', 'any_of', 'none_of', 'not_'):
+        print()
         return batch_row_handling(getattr(anvil.tables.query, attr))
     else:
         return getattr(anvil.tables.query, attr)
@@ -12,6 +14,7 @@ def __getattr__(attr):
 def batch_row_handling(cls):
     original_init = cls.__init__
 
+    @wraps(original_init)
     def new_init(self, *args, **kwargs):
         debatchified_args = [debatchify(arg) for arg in args]
         debatchified_kwargs = {column: debatchify(value) for column, value in kwargs.items()}
