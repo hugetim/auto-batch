bc4e02e143d4f3f5a1f5a59efb583b35ed7a603d
hugetim@gmail.com <hugetim@gmail.com>
2024-07-15 17:50:17 +0000

    Add USE_WRAPPED toggle to users

---
+++ client_code/_users.py
@@ -0,0 +1,12 @@
+from .auto_batch import debatchify_inputs, BatchTable
+import anvil.users
+
+
+def force_login(*args, **kwargs):
+    return debatchify_inputs(anvil.users.force_login)(*args, **kwargs)
+
+
+def get_user(*args, **kwargs):
+    user_row = anvil.users.get_user(*args, **kwargs)
+    batch_table = BatchTable.of_row(user_row)
+    return batch_table.get_batch_row(user_row)
--- client_code/users.py
+++ client_code/users.py
@@ -1,16 +1,10 @@
-from .auto_batch import debatchify_inputs, BatchTable
+from .auto_batch import USE_WRAPPED
+from . import _users
 import anvil.users
 
 
-def force_login(*args, **kwargs):
-    return debatchify_inputs(anvil.users.force_login)(*args, **kwargs)
-
-
-def get_user(*args, **kwargs):
-    user_row = anvil.users.get_user(*args, **kwargs)
-    batch_table = BatchTable.of_row(user_row)
-    return batch_table.get_batch_row(user_row)
-
-
 def __getattr__(attr):
-    return getattr(anvil.users, attr)
+    if USE_WRAPPED and attr in ('force_login', 'get_user'):
+        return getattr(_users, attr)
+    else:
+        return getattr(anvil.users, attr)
